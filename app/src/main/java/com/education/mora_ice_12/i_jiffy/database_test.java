61043492cdbdb9b02f7a8c01cb7c0c826e03cacb
KRVPerera <rukshan.viduranga@gmail.com>
2015-03-03 17:50:52 +0530

    Fixing navigation through views
    Signed-off-by: KRVPerera <rukshan.viduranga@gmail.com>

---
+++ app/src/main/java/com/education/mora_ice_12/i_jiffy/database_test.java
@@ -0,0 +1,17 @@
+package com.education.mora_ice_12.i_jiffy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by krv on 3/3/2015.
+ */
+public class database_test extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.database_test);
+
+    }
+
+}
